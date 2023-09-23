ecedba443c1ec876186c0db85d97aa3493137109
Benjamín Balun <benjamin.balun@gmail.com>
2023-09-23 23:30:48 +0200

    Added retry to warehouseService calls

--- src/services/warehouseService.ts
+++ src/services/warehouseService.ts
@@ -1,4 +1,5 @@
 import wretch from "wretch";
+import { retry } from "wretch/middlewares";
 
 export type ProductPositionData = {
   positionId: string;
@@ -9,16 +10,18 @@ export type ProductPositionData = {
   quantity: number;
 };
 
-const api = wretch(process.env.WAREHOUSE_API_BASE_URL).headers({
-  "x-api-key": process.env.WAREHOUSE_API_KEY!,
-});
+const api = wretch(process.env.WAREHOUSE_API_BASE_URL)
+  .headers({
+    "x-api-key": process.env.WAREHOUSE_API_KEY!,
+  })
+  .middlewares([
+    retry({
+      maxAttempts: 3,
+    }),
+  ]);
 
 export async function fetchProductPositions(productId: string) {
-  // TODO: error handling
-  // TODO: retry
-  const data = await api.get(`/products/${productId}/positions`).json<ProductPositionData[]>();
-
-  return data;
+  return await api.get(`/products/${productId}/positions`).json<ProductPositionData[]>();
 }
 
 export async function fetchManyProductPositions(productIds: string[]) {
