fd0c03083d5e874e0aecf47c4772ad499da5fde6
Benjamín Balun <benjamin.balun@gmail.com>
2023-09-23 22:37:10 +0200

    Added basic request validation and logging

--- index.ts
+++ index.ts
@@ -1,46 +1,56 @@
 import "dotenv/config";
-import * as warehouseService from "./src/services/warehouseSerice";
+import * as warehouseService from "./src/services/warehouseService";
 
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { cartesianProduct, permutations } from "combinatorial-generators";
+import { z } from "zod";
+import pino from "pino-http";
 
 const app = express();
 
-type Point = {
-  x: number;
-  y: number;
-  z: number;
-};
-
-type Order = {
-  products: string[];
-  startingPosition: Point;
-};
-
+app.use(pino());
 app.use(express.json());
 
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  req.log.error({ message: "Uncaught exception occurred", error: err });
+  res.status(500).json({ message: "Something went wrong." });
+});
+
+// TODO: move into separate file with its handler
+const orderValidator = z.object({
+  products: z.array(z.string().nonempty()).nonempty(),
+  startingPosition: z.object({
+    x: z.number().nonnegative(),
+    y: z.number().nonnegative(),
+    z: z.number().nonnegative(),
+  }),
+});
+
+// TODO: error handling - 500 - oops something went wrong + structure logging (pina)
+
 app.post("/order/optimize", async (req, res) => {
-  // TODO: validate input
-  const body = req.body as Order;
-  // TODO: error handling
-  // TODO: check for duplicates productIds
-  const productsWithPositionData = await Promise.all(
-    body.products.map(async (product) => {
-      const positions = await warehouseService.fetchProductPositions(product);
-      return {
-        product,
-        positions,
-      };
-    })
-  );
+  const orderValidationResult = orderValidator.safeParse(req.body);
+  if (!orderValidationResult.success) {
+    return res.status(400).json(orderValidationResult.error);
+  }
+  const order = orderValidationResult.data;
+
+  const uniqueProducts = new Set(order.products);
+  if (uniqueProducts.size !== order.products.length) {
+    req.log.warn({ order }, "Order contains duplicate products. Duplicates were removed.");
+    order.products = [...uniqueProducts] as [string, ...string[]];
+  }
+
+  // TODO: error handling + logging
+  const productsWithPositionData = await warehouseService.fetchManyProductPositions(order.products);
 
   const startingPosition = {
     positionId: "__start-position-id",
     productId: "",
     quantity: 0,
-    x: body.startingPosition.x,
-    y: body.startingPosition.y,
-    z: body.startingPosition.z,
+    x: order.startingPosition.x,
+    y: order.startingPosition.y,
+    z: order.startingPosition.z,
   };
   const positions = productsWithPositionData.flatMap((p) => p.positions);
   positions.push(startingPosition);
@@ -75,51 +85,21 @@ app.post("/order/optimize", async (req, res) => {
   });
 });
 
-/**
- * Generate all sets of product positions.
- */
-// TODO: optimize: get rid of unnecessary slicing
-// function generateProductCombinations(
-//   products: {
-//     product: string;
-//     positions: warehouseService.PositionData[];
-//   }[]
-// ): warehouseService.PositionData[][] {
-//   if (products.length === 0) {
-//     return [];
-//   }
-
-//   if (products.length === 1) {
-//     return products[0].positions.map((x) => [x]);
-//   }
-
-//   const firstProduct = products[0];
-//   const remainingProducts = products.slice(1);
-
-//   const res = [] as warehouseService.PositionData[][];
-
-//   for (const firstProductPosition of firstProduct.positions) {
-//     for (const otherCombinations of generateProductCombinations(remainingProducts)) {
-//       res.push([firstProductPosition, ...otherCombinations]);
-//     }
-//   }
-
-//   return res;
-// }
 function generateProductCombinations(
   productsWithPositionData: {
     product: string;
-    positions: warehouseService.PositionData[];
+    positions: warehouseService.ProductPositionData[];
   }[]
 ) {
   return cartesianProduct(...productsWithPositionData.map((x) => x.positions));
 }
 
 // TODO: move to utils
-type From = string;
-type To = string;
-type AdjacencyMatrix = Record<From, Record<To, number>>;
-function calculateAdjacencyMatrix(positions: warehouseService.PositionData[]): AdjacencyMatrix {
+type FromPositionId = string;
+type ToPositionId = string;
+type Distance = number;
+type AdjacencyMatrix = Record<FromPositionId, Record<ToPositionId, Distance>>;
+function calculateAdjacencyMatrix(positions: warehouseService.ProductPositionData[]): AdjacencyMatrix {
   const adjacencyMatrix: AdjacencyMatrix = {};
 
   for (const from of positions) {
@@ -133,6 +113,12 @@ function calculateAdjacencyMatrix(positions: warehouseService.PositionData[]): A
 
   return adjacencyMatrix;
 }
+
+type Point = {
+  x: number;
+  y: number;
+  z: number;
+};
 
 // TODO: move to utils
 function distance(a: Point, b: Point): number {
